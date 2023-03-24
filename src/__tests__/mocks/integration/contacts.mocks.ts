da4e47a7e5b9b9b5b32e9811a69a2f5e5ff12240
unknown <dreicleal@hotmail.com>
2023-03-24 14:20:28 -0300

    test: contact mocks

---
+++ src/__tests__/mocks/integration/contacts.mocks.ts
@@ -0,0 +1,14 @@
+import { IContact } from "../../../interfaces/contacts.interface";
+
+
+export const mockedCreateContact: IContact ={
+    email: "contact_default@meail.com",
+    fullName: "Contact Test",
+    phone: "996875425"
+}
+
+export const mockedCreateToUpdateContact: IContact ={
+    email: "contact_to_update@meail.com",
+    fullName: "Contact To Update",
+    phone: "996875425"
+}
