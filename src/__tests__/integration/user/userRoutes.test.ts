ad28947757794df8cea52cdd692a91f62dc6252a
unknown <dreicleal@hotmail.com>
2023-03-24 18:11:29 -0300

    tests: contact routes

--- src/__tests__/integration/user/userRoutes.test.ts
+++ src/__tests__/integration/user/userRoutes.test.ts
@@ -18,97 +18,101 @@ describe("/users", () => {
     await connection.destroy();
   });
 
-  test("POST /users -> Should NOT be able to create an account without email field", async () =>{
-    const {fullName, password, phone} = mockedCreatedUser
+  test("POST /users -> Should NOT be able to create an account without email field", async () => {
+    const { fullName, password, phone } = mockedCreatedUser;
     const userWithoutEmail = {
-        fullName:fullName,
-        password: password,
-        phone: phone,
-
-    }
+      fullName: fullName,
+      password: password,
+      phone: phone,
+    };
     const response = await request(app).post("/users").send(userWithoutEmail);
 
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty("email")
-    expect(response.body.email[0]).toEqual("Required")
-  })
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("email");
+    expect(response.body.email[0]).toEqual("Required");
+  });
 
-  test("POST /users -> Should NOT be able to create an account without fullName field", async () =>{
-    const {email, password, phone} = mockedCreatedUser
+  test("POST /users -> Should NOT be able to create an account without fullName field", async () => {
+    const { email, password, phone } = mockedCreatedUser;
     const userWithoutFullName = {
-        email:email,
-        password: password,
-        phone: phone,
+      email: email,
+      password: password,
+      phone: phone,
+    };
+    const response = await request(app)
+      .post("/users")
+      .send(userWithoutFullName);
 
-    }
-    const response = await request(app).post("/users").send(userWithoutFullName);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("fullName");
+    expect(response.body.fullName[0]).toEqual("Required");
+  });
 
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty("fullName")
-    expect(response.body.fullName[0]).toEqual("Required")
-  })
-
-  test("POST /users -> Should NOT be able to create an account without phone field", async () =>{
-    const {email, password, fullName} = mockedCreatedUser
+  test("POST /users -> Should NOT be able to create an account without phone field", async () => {
+    const { email, password, fullName } = mockedCreatedUser;
     const userWithoutPhone = {
-        email:email,
-        password: password,
-        fullName: fullName,
-
-    }
+      email: email,
+      password: password,
+      fullName: fullName,
+    };
     const response = await request(app).post("/users").send(userWithoutPhone);
 
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty("phone")
-    expect(response.body.phone[0]).toEqual("Required")
-  })
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("phone");
+    expect(response.body.phone[0]).toEqual("Required");
+  });
 
-  test("POST /users -> Should NOT be able to create an account without password field", async () =>{
-    const {email, fullName, phone} = mockedCreatedUser
+  test("POST /users -> Should NOT be able to create an account without password field", async () => {
+    const { email, fullName, phone } = mockedCreatedUser;
     const userWithoutPassword = {
-        email:email,
-        fullName: fullName,
-        phone: phone,
+      email: email,
+      fullName: fullName,
+      phone: phone,
+    };
+    const response = await request(app)
+      .post("/users")
+      .send(userWithoutPassword);
 
-    }
-    const response = await request(app).post("/users").send(userWithoutPassword);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("password");
+    expect(response.body.password[0]).toEqual("Required");
+  });
 
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty("password")
-    expect(response.body.password[0]).toEqual("Required")
-  })
-
-  test("POST /users -> Should NOT be able to create an account without a valid password", async () =>{
-    const {email, fullName, phone} = mockedCreatedUser
+  test("POST /users -> Should NOT be able to create an account without a valid password", async () => {
+    const { email, fullName, phone } = mockedCreatedUser;
     const userWithInvalidPassword = {
-        email:email,
-        fullName: fullName,
-        phone: phone,
-        password:"123"
+      email: email,
+      fullName: fullName,
+      phone: phone,
+      password: "123",
+    };
+    const response = await request(app)
+      .post("/users")
+      .send(userWithInvalidPassword);
 
-    }
-    const response = await request(app).post("/users").send(userWithInvalidPassword);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("password");
+    expect(response.body.password[0]).toEqual(
+      "Password need to be at least 4 characters"
+    );
+  });
 
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty("password")
-    expect(response.body.password[0]).toEqual("Password need to be at least 4 characters")
-  })
-
-  test("POST /users -> Should NOT be able to create an account without a valid e-mail field", async () =>{
-    const { fullName, phone, password} = mockedCreatedUser
+  test("POST /users -> Should NOT be able to create an account without a valid e-mail field", async () => {
+    const { fullName, phone, password } = mockedCreatedUser;
     const userWithInvalidEmail = {
-        email:"invalid_mail.com",
-        fullName: fullName,
-        phone: phone,
-        password: password
+      email: "invalid_mail.com",
+      fullName: fullName,
+      phone: phone,
+      password: password,
+    };
+    const response = await request(app)
+      .post("/users")
+      .send(userWithInvalidEmail);
 
-    }
-    const response = await request(app).post("/users").send(userWithInvalidEmail);
-
-    expect(response.statusCode).toBe(400)
-    expect(response.body).toHaveProperty("email")
-    expect(response.body.email[0]).toEqual("Invalid e-mail format")
-  })
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("email");
+    expect(response.body.email[0]).toEqual("Invalid e-mail format");
+  });
 
   test("POST /users -> Should be able to create an account", async () => {
     const response = await request(app).post("/users").send(mockedCreatedUser);
@@ -116,39 +120,43 @@ describe("/users", () => {
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("fullName");
     expect(response.body).toHaveProperty("email");
+    expect(response.body).not.toHaveProperty("password");
     expect(response.body).toHaveProperty("secondaryEmail");
     expect(response.body).toHaveProperty("phone");
     expect(response.body).toHaveProperty("profileImg");
     expect(response.body).toHaveProperty("createdAt");
     expect(response.body).toHaveProperty("deletedAt");
 
-    expect(response.body.email).toEqual(mockedCreatedUser.email)
-    expect(response.body.fullName).toEqual(mockedCreatedUser.fullName)
-    expect(response.body.phone).toEqual(mockedCreatedUser.phone)
+    expect(response.body.email).toEqual(mockedCreatedUser.email);
+    expect(response.body.fullName).toEqual(mockedCreatedUser.fullName);
+    expect(response.body.phone).toEqual(mockedCreatedUser.phone);
     expect(response.status).toBe(201);
   });
 
-  test("POST /users -> Should NOT be able to create an account that already exists", async () =>{
+  test("POST /users -> Should NOT be able to create an account that already exists", async () => {
     const response = await request(app).post("/users").send(mockedCreatedUser);
 
-    expect(response.statusCode).toBe(409)
-    expect(response.body).toHaveProperty("message")
+    expect(response.statusCode).toBe(409);
+    expect(response.body).toHaveProperty("message");
   });
 
-  test("GET /users/user -> Should NOT be able to list your profile without Token", async ()=> {
-    const response = await request(app).get("/users/user")
+  test("GET /users/user -> Should NOT be able to list your profile without Token", async () => {
+    const response = await request(app).get("/users/user");
 
-    expect(response.body).toHaveProperty("message")
-    expect(response.body.message).toEqual("Token is missing")
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual("Token is missing");
     expect(response.status).toBe(401);
   });
 
-  test("GET /users/user -> Should be able to list your own profile", async ()=> {
-    const response = await request(app).get("/users/user").set("Authorization", await token())
-    
+  test("GET /users/user -> Should be able to list your own profile", async () => {
+    const response = await request(app)
+      .get("/users/user")
+      .set("Authorization", await token());
+
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("fullName");
     expect(response.body).toHaveProperty("email");
+    expect(response.body).not.toHaveProperty("password");
     expect(response.body).toHaveProperty("secondaryEmail");
     expect(response.body).toHaveProperty("phone");
     expect(response.body).toHaveProperty("profileImg");
@@ -157,37 +165,38 @@ describe("/users", () => {
     expect(response.body).toHaveProperty("contacts");
     expect(response.body.contacts).toHaveProperty("length");
 
-    expect(response.body.email).toEqual(mockedCreatedUser.email)
-    expect(response.body.fullName).toEqual(mockedCreatedUser.fullName)
-    expect(response.body.phone).toEqual(mockedCreatedUser.phone)
+    expect(response.body.email).toEqual(mockedCreatedUser.email);
+    expect(response.body.fullName).toEqual(mockedCreatedUser.fullName);
+    expect(response.body.phone).toEqual(mockedCreatedUser.phone);
     expect(response.status).toBe(200);
   });
 
-  test("PATCH /users -> Should NOT be able to update profile without authentication", async ()=> {
+  test("PATCH /users -> Should NOT be able to update profile without authentication", async () => {
     const newUserData = {
-        email: "updated_email@email.com",
-        password:"abcde",
-        profileImg: "http://imagerandom.com",
-        phone: "995687523"
+      email: "updated_email@email.com",
+      password: "abcde",
+      profileImg: "http://imagerandom.com",
+      phone: "995687523",
+    };
+    const response = await request(app).patch("/users").send(newUserData);
 
-    }
-    const response = await request(app).patch("/users").send(newUserData)
-    
     expect(response.body).toHaveProperty("message");
     expect(response.status).toBe(401);
   });
 
-  test("PATCH /users -> Should be able to update profile", async ()=> {
+  test("PATCH /users -> Should be able to update profile", async () => {
     const newUserData = {
-        email: "updated_email@email.com",
-        password:"abcde",
-        profileImg: "http://imagerandom.com",
-        phone: "995687523"
+      email: "updated_email@email.com",
+      password: "abcde",
+      profileImg: "http://imagerandom.com",
+      phone: "995687523",
+    };
 
-    }
+    const response = await request(app)
+      .patch("/users")
+      .send(newUserData)
+      .set("Authorization", await updateUserToken());
 
-    const response = await request(app).patch("/users").send(newUserData).set("Authorization", await updateUserToken())
-    
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("fullName");
     expect(response.body).toHaveProperty("email");
@@ -197,40 +206,42 @@ describe("/users", () => {
     expect(response.body).toHaveProperty("createdAt");
     expect(response.body).toHaveProperty("deletedAt");
 
-    expect(response.body.email).toEqual(newUserData.email)
-    expect(response.body.profileImg).toEqual(newUserData.profileImg)
-    expect(response.body.phone).toEqual(newUserData.phone)
+    expect(response.body.email).toEqual(newUserData.email);
+    expect(response.body.profileImg).toEqual(newUserData.profileImg);
+    expect(response.body.phone).toEqual(newUserData.phone);
     expect(response.status).toBe(200);
   });
 
   test("POST /login -> Should be able to login with new credentials", async () => {
-    const response = await request(app).post("/login")
-    .send({email:"updated_email@email.com", password:"abcde"});
+    const response = await request(app)
+      .post("/login")
+      .send({ email: "updated_email@email.com", password: "abcde" });
 
     expect(response.body).toHaveProperty("token");
     expect(response.statusCode).toBe(200);
   });
 
-  test("DELETE /users -> Should NOT be able to delete an account without authentication", async ()=> {
-    const response = await request(app).delete("/users")
+  test("DELETE /users -> Should NOT be able to delete an account without authentication", async () => {
+    const response = await request(app).delete("/users");
 
-    expect(response.body).toHaveProperty("message")
-    expect(response.body.message).toEqual("Token is missing")
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual("Token is missing");
     expect(response.status).toBe(401);
-  })
+  });
 
-  test("DELETE /users -> Should be able to delete an account", async ()=> {
-    const response = await request(app).delete("/users").set("Authorization", await token())
+  test("DELETE /users -> Should be able to delete an account", async () => {
+    const response = await request(app)
+      .delete("/users")
+      .set("Authorization", await token());
 
     expect(response.status).toBe(204);
-  })
+  });
 
-  test("POST /login -> Should NOT be able to login with a deleted account", async ()=> {
-    const response = await request(app).post("/login").send(mockedLoginRequest)
+  test("POST /login -> Should NOT be able to login with a deleted account", async () => {
+    const response = await request(app).post("/login").send(mockedLoginRequest);
 
-    expect(response.body).toHaveProperty("message")
-    expect(response.body.message).toEqual("Wrong email or password")
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual("Wrong email or password");
     expect(response.status).toBe(401);
-  })
-
+  });
 });
